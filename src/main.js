23db61b7a5c01f6bc0536f976a6057cdbee7e94d
sedighi-mahdi <sedighi.mahdi87@gmail.com>
2019-10-01 13:17:54 +0330

    change header

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  
 }).$mount('#app')
+/*new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+}).$mount('#app')*/
