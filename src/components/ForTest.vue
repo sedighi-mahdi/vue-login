154f9b5571544c615d6e968a275555901e3ae01c
sedighi-mahdi <sedighi.mahdi87@gmail.com>
2019-10-02 09:13:40 +0330

    add iftest and for test

---
+++ src/components/ForTest.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <table class="table table-striped">
+        <tr v-for="(usr,index) in users" :key="usr.id">
+            <td>{{index}}</td>
+            <td>{{usr.fname}}</td>
+            <td>{{usr.lname}}</td>
+        </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "for-test",
+  data() {
+    return {
+      users:[
+          {
+              id:1,
+              fname:"ali",
+              lname:"ali nia"
+          },
+          {
+              id:2,
+              fname:"mahdi",
+              lname:"sedighi"
+          },
+          {
+              id:3,
+              fname:"zahra",
+              lname:"akbari"
+          }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/IFTest.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div v-if="isA">{{msgA}}</div>
+    <div v-else>{{msgB}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "if-test",
+  data() {
+    return {
+      isA: false,
+      msgA: "block a",
+      msgB: "block b"
+    };
+  }
+};
+</script>
+
+<style scoped></style>
