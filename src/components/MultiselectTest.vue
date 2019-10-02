2552a27cf6f712e980dff6b850dde2997a90ee08
sedighi-mahdi <sedighi.mahdi87@gmail.com>
2019-10-02 10:50:58 +0330

    add multiselect file

---
+++ src/components/MultiselectTest.vue
@@ -0,0 +1,42 @@
+<!-- Vue component -->
+<template>
+  <div>
+    <label class="typo__label">Simple select / dropdown</label>
+    <multiselect v-model="value" :options="options" :multiple="false" :close-on-select="true" 
+    :clear-on-select="false" :preserve-search="true" 
+    placeholder="Pick some" label="name" track-by="name" :preselect-first="true">
+    </multiselect>
+  <pre class="language-json"><code>{{ value  }}</code></pre>
+  </div>
+</template>
+
+<script>
+  
+  import Multiselect from 'vue-multiselect'
+
+  export default {
+    // OR register locally
+    components: { Multiselect },
+    data () {
+      return {
+        value: [],
+        options: [
+        { name: 'Vue.js', language: 'JavaScript' },
+        { name: 'Adonis', language: 'JavaScript' },
+        { name: 'Rails', language: 'Ruby' },
+        { name: 'Sinatra', language: 'Ruby' },
+        { name: 'Laravel', language: 'PHP' },
+        { name: 'Phoenix', language: 'Elixir' }
+      ]
+      }
+    }
+  }
+</script>
+
+<!-- New step!
+     Add Multiselect CSS. Can be added as a static asset or inside a component. -->
+
+
+<style>
+  
+</style>
